92a94035f8aaca4c0a0bc4d458091d26f4e5a7a2
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-06 13:27:13 +0300

    Now completed the collaboration section

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Section from './Section'
 import {collabApps, collabContent, collabText} from '../constants'
 import { brainwaveSymbol, check } from '../assets'
+import {LeftCurve , RightCurve} from './design/Collaboration'
 import Button from './Button'
 
 const Collaboration = () => {
@@ -29,7 +30,7 @@ const Collaboration = () => {
             </div>
 
             <div className='lg:ml-auto xl:w-[38rem] mt-4'>
-                <p className='body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto'>{collabText}</p>
+                <p className='body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto'>{collabText}</p>
 
                 {/* The orbit starts from here the largest orbit */}
                 <div className='relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100'>
@@ -58,6 +59,8 @@ const Collaboration = () => {
                         </li>
                     ))}
                 </ul>
+                <LeftCurve />
+                <RightCurve/>
                 </div>
             </div>
         </div>
